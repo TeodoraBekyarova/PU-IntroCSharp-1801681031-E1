d3088886cf0addc0452cec9c4e3c86096aa00914
SimonaIvanova7 <tydi18@abv.bg>
2018-10-30 14:25:41 +0200

    1

---
+++ ExamOne1801681031/RectangleAria/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RectangleAria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въдеди а:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Въведи б:");
+            int b = int.Parse(Console.ReadLine());
+
+            double S = a * b;
+
+            Console.WriteLine("Лицето на правоъгълника е:" + S);
+            Console.ReadLine();
+        }
+    }
+}
