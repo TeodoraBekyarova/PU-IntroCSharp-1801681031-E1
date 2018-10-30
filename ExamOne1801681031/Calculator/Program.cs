32a5de44cd3daf0302df69444836d6cde32ca9e2
SimonaIvanova7 <tydi18@abv.bg>
2018-10-30 15:25:12 +0200

    3

---
+++ ExamOne1801681031/Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want to calculate some numbers? (yes or no) ");
+            Console.ReadLine();
+
+            Console.WriteLine("Enter first number:");
+            double number1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            double number2 = double.Parse(Console.ReadLine());
+
+            string symbole = Console.ReadLine();
+
+            switch (symbole) // +, -, *, /
+            {
+                case "+":
+                    Console.WriteLine("You did it!");
+                    break;
+                case "-":
+                    Console.WriteLine("Try something else!");
+                    break;
+                case "*":
+                    Console.WriteLine("Try with plus!");
+                    break;
+                case "/":
+                    Console.WriteLine("Good job!");
+                    break;
+
+            }
+        }
+    }
+}
---
+++ ExamOne1801681031/PiggyBank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PiggyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Не ми стигна времето");
+        }
+    }
+}
